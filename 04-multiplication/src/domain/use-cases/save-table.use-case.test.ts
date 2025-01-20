8c38625f953911f98a7aedf4bba4bc64e81660cf
Fabztech <fabztech@outlook.com>
2025-01-20 14:01:27 -0500

    Save Table Case Use Test - with options

--- 04-multiplication/src/domain/use-cases/save-table.use-case.test.ts
+++ 04-multiplication/src/domain/use-cases/save-table.use-case.test.ts
@@ -3,13 +3,32 @@ import fs from 'fs';
 describe('Save Table Case Use Test', ()=>{
     
     afterEach(()=> {
-        fs.rmSync('outputs/DefaultTable.txt');
+        // fs.rmSync('custom-outputs', { recursive: true });
     })
 
     test('should create a file', ()=>{
         const saveTableOptions = { tableContent: 'Default content' };
         const resultSaveTable = new SaveTable().execute( saveTableOptions );
         const path = 'outputs/DefaultTable.txt';
+        
+        const fileExist = fs.readFileSync(path); 
+        const fileContent = fs.readFileSync(path, {encoding: 'utf-8'});
+        
+        expect(resultSaveTable).toBeTruthy();
+        expect(fileExist).toBeTruthy();
+        expect(fileContent).toBe(saveTableOptions.tableContent);
+        
+    })
+
+    test('should save a file with custom values', ()=>{
+        const saveTableOptions = { 
+            tableContent: 'Default content',
+            fileDest: 'custom-outputs/file-destination/',
+            fileName: 'custom-table-name' 
+        };
+        const resultSaveTable = new SaveTable().execute( saveTableOptions );
+        const path = saveTableOptions.fileDest+saveTableOptions.fileName+'.txt';
+        
         
         const fileExist = fs.readFileSync(path); 
         const fileContent = fs.readFileSync(path, {encoding: 'utf-8'});
