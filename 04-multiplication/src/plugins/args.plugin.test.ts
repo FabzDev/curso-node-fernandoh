94b1d0822a77fa0b567f94e7403771c120a72577
Fabztech <fabztech@outlook.com>
2025-01-20 23:37:26 -0500

    args Plugin Use Test - jest.resetModules()

--- 04-multiplication/src/plugins/args.plugin.test.ts
+++ 04-multiplication/src/plugins/args.plugin.test.ts
@@ -5,11 +5,17 @@ const runCommand = async (args: string[]) => {
 };
 
 describe("Args Pluging Test", () => {
+  const originalArgv = process.argv;
+
+  beforeEach(() => {
+    process.argv = originalArgv;
+    jest.resetModules();
+  });
 
   test("temporal test", async () => {
     const myArgs = await runCommand(["-b", "5"]);
-
-    expect(myArgs).toEqual(expect.objectContaining({
+    expect(myArgs).toEqual(
+      expect.objectContaining({
         b: 5,
         l: 10,
         n: "defaultTable",
@@ -18,4 +24,18 @@ describe("Args Pluging Test", () => {
       })
     );
   });
+
+  test("temporal test 2", async () => {
+    const myArgs = await runCommand(["-b", "4", "-l", "9", "-s"]);
+    expect(myArgs).toEqual(
+      expect.objectContaining({
+        b: 4,
+        l: 9,
+        n: "defaultTable",
+        d: "outputs/",
+        s: true,
+      })
+    );
+  });
+
 });
